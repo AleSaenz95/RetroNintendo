5fa031598fbd7fbbbec0ae36824a1e803585cd20
AleSaenz95 <saenz2595@gmail.com>
2024-12-06 12:43:45 -0600

    cambios pwa

--- sw.js
+++ sw.js
@@ -1,43 +1,37 @@
-const CACHE_NAME = "retronintendo-cache-v1";
-const urlsToCache = [
-    "/",
-    "/static/css/estilos.css",
-    "/static/media/classic-games.jpg",
-    "/static/media/worldwide-shipping.jpg",
-    "/static/media/welcome.jpg",
-    "/static/media/icon-192x192.png",
-    "/static/media/icon-512x512.png"
-];
+const CACHE_NAME = "RetroNintendo-cache-v1";
+const urlsToCache = ["/", "/index.html", "/css/styles.css", "/js/app.js", "/images/icons/icon-192x192.png"];
 
-// Instalación del Service Worker
+// Instalar el Service Worker y cachear recursos
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Archivos cacheados");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// Activación del Service Worker
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Intercepción de solicitudes
+// Interceptar solicitudes y responder desde la caché
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Actualizar la caché
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      )
+    )
+  );
 });
